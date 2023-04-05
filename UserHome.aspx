7db24f9ec348c4c0dd585ea451f2473271ad1d99
carlossosIT <denisonjose@gmail.com>
2023-04-05 18:25:01 -0400

    subida del proyecto inicial con algunos ajustes
    subida version del proyecto inicial
    
    tiene login y tiene un ejemplo de llenar la lista de empresas y sucursal

---
+++ UserHome.aspx
@@ -0,0 +1,47 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" MasterPageFile="~/GeneralLayout.master" CodeFile="UserHome.aspx.cs" Inherits="UserHome" %>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="Server">
+    <div class="container">
+        <asp:Label ID="lblSuccess" runat="server" CssClass="text-success"></asp:Label>
+
+        <nav class="navbar navbar-expand-lg navbar-light ">
+            <%--                <a class="navbar-brand" href="Default.aspx">
+                    <img alt="Logo" src="Images/tc only 512.png" height="30" />
+                    TechCybo.com
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>--%>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Empresas - Sucusales
+                            </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <%--<a class="dropdown-item" href="Products.aspx">All Products</a>
+                                <div class="dropdown-divider"></div>--%>
+
+                            <asp:Repeater ID="rptCategory" runat="server" OnItemDataBound="OnItemDataBound">
+                                <ItemTemplate>
+                                    <a class="dropdown-item main-cat" ><%# Eval("Co_empresa") %></a>
+                                    <asp:HiddenField ID="hfCo_empresa" runat="server" Value='<%# Eval("Co_empresa") %>' />
+
+                                    <asp:Repeater ID="rptSubCategories" runat="server">
+                                        <ItemTemplate>
+                                            <a class="dropdown-item" href="Products.aspx?subcat=<%# Eval("Co_sucur") %>"><%# Eval("Co_sucur") %>-<%# Eval("Sucur_des") %></a>
+                                        </ItemTemplate>
+                                        <FooterTemplate>
+                                            <div class="dropdown-divider"></div>
+                                        </FooterTemplate>
+                                    </asp:Repeater>
+
+                                </ItemTemplate>
+                            </asp:Repeater>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+</asp:Content>
---
+++ UserHome.aspx.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using WsLogin;
+using WsClientes;
+
+public partial class UserHome : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Session["USERNAME"] != null)
+        {
+            lblSuccess.Text = "Login Success, Welcome " + Session["USERNAME"].ToString() + "";
+        }
+        BindCategories();
+    }
+
+    public void BindCategories()
+    {
+        UsuarioOut UO = ((UsuarioOut)Session["U_OUT"]);
+
+        rptCategory.DataSource = UO.Slista_Empresas;
+        rptCategory.DataBind();
+        /*
+        String CS = ConfigurationManager.ConnectionStrings["MyDatabaseConnectionString1"].ConnectionString;
+        using (SqlConnection con = new SqlConnection(CS))
+        {
+            using (SqlCommand cmd = new SqlCommand("select * from tblCategories", con))
+            {
+                cmd.CommandType = CommandType.Text;
+                using (SqlDataAdapter sda = new SqlDataAdapter(cmd))
+                {
+                    DataTable dtBrands = new DataTable();
+                    sda.Fill(dtBrands);
+                    rptCategory.DataSource = dtBrands;
+                    rptCategory.DataBind();
+                }
+
+            }
+        }*/
+    }
+
+    protected void OnItemDataBound(object sender, RepeaterItemEventArgs e)
+    {
+        if (e.Item.ItemType == ListItemType.Item || e.Item.ItemType == ListItemType.AlternatingItem)
+        {
+            string co_empresa = (e.Item.FindControl("hfCo_empresa") as HiddenField).Value;
+            Repeater rptSubCategories = e.Item.FindControl("rptSubCategories") as Repeater;
+
+            WsClientes.WsClientes wsc = new WsClientes.WsClientes();
+            SucursalIn Sucur = new SucursalIn();
+            Sucur.Co_usuario = ((UsuarioOut)Session["U_OUT"]).SUsuario_Codigo;
+            Sucur.Co_empresa = co_empresa;
+            try
+            {
+                SucursalOut[] arr_sucur = wsc.ConsultarSucursales(Sucur);
+                if (arr_sucur.Length > 0)
+                {
+                    rptSubCategories.DataSource = arr_sucur;
+                    rptSubCategories.DataBind();
+                }
+            }
+            catch (Exception ex)
+            { }
+            /*String CS = ConfigurationManager.ConnectionStrings["MyDatabaseConnectionString1"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(CS))
+            {
+                using (SqlCommand cmd = new SqlCommand(string.Format("SELECT * FROM tblSubCategories WHERE MainCatID='{0}'", catId), con))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    using (SqlDataAdapter sda = new SqlDataAdapter(cmd))
+                    {
+                        DataTable dtBrands = new DataTable();
+                        sda.Fill(dtBrands);
+                        rptSubCategories.DataSource = dtBrands;
+                        rptSubCategories.DataBind();
+                    }
+
+                }
+            }*/
+        }
+    }
+}
